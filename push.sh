95affd87e5003638fabbb5d4026e2fa3240cb24a
yori <3070960636@qq.com>
2023-09-28 08:56:13 +0800

    自动推送

--- push.sh
+++ push.sh
@@ -1,5 +1,5 @@
-git config --global user.name 'yangjh-xbmu'
-git config --global user.email 'yangjh@yeah.net'
+git config --global user.name 'yori'
+git config --global user.email '3070960636@qq.com'
 git config --global http.sslverify false
 git add .
 git commit -m '自动推送'
