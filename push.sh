47d662bf44105743161ee1f8392d30bb1cad86a2
yori <3070960636@qq.com>
2023-11-25 10:47:58 +0800

    自动推送

--- push.sh
+++ push.sh
@@ -1,6 +1,6 @@
 git config --global user.name 'yori'
 git config --global user.email '3070960636@qq.com'
 git config --global http.sslverify false
-git add .
+git add 
 git commit -m '自动推送'
 git push
